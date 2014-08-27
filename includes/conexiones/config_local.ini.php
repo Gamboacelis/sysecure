e4d298da0762b20d12831bf1df2cc3c8d50e1170
dannypads <dannypads@hotmail.com>
2014-08-26 22:43:51 -0500

    actualizacion de control2

--- includes/conexiones/config_local.ini.php
+++ includes/conexiones/config_local.ini.php
@@ -6,9 +6,9 @@ define('SISTEM_NAME','sysecure');
 define('PATH_PPL','/uploads/imagenes/ppl/');
 define('PATH_VISITAS','/uploads/imagenes/visitas/');
 define('HOST_NAME','localhost');
-define('USER_NAME', 'bulkenco_wie');
+define('USER_NAME', 'root');
 //define('USER_NAME', 'root');
-define('USER_PASSWD', 'mvTmjsunp00');
+define('USER_PASSWD', '');
 //define('USER_PASSWD', '');
-define('DB_NAME','bulkenco_sysecure');
+define('DB_NAME','sysecure');
 ?>
