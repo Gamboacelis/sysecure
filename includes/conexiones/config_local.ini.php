75878e6b4134293ce0b61224f439c34c1b20d7af
Willian Espinosa <wespinosa@MacBook-Pro-de-Willian.local>
2014-11-10 21:04:14 -0500

    Cambios locales

--- includes/conexiones/config_local.ini.php
+++ includes/conexiones/config_local.ini.php
@@ -1,11 +1,11 @@
 <?php
 
-define('PATH_PROD', 'D:/Compartida/htdocs/');
+define('PATH_PROD', '/Applications/AMPPS/www/');
 define('SISTEM_NAME', 'sysecure');
 define('PATH_PPL', '/uploads/imagenes/ppl/');
 define('PATH_VISITAS', '/uploads/imagenes/visitas/');
 define('HOST_NAME', 'localhost');
 define('USER_NAME', 'root');
-define('USER_PASSWD', '');
+define('USER_PASSWD', 'mysql');
 define('DB_NAME', 'bulkenco_sysecure');
 
